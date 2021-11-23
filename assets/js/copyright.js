8f388294c85158cdc3964e2146455497aabc1e84
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-11-22 22:15:08 -0800

    Update copyright.js

--- assets/js/copyright.js
+++ assets/js/copyright.js
@@ -1,1 +1,1 @@
-document.body.onload=function(){document.getElementById("copyright").innerHTML="Copyright &copy; "+(new Date).getFullYear()+" <strong>Steadfast5</strong>. All rights reserved."};
+document.body.onload=function(){document.getElementById("copyright").innerHTML="Copyright &copy; "+(new Date).getFullYear()+" <strong>Steadfast5</strong>."};
